6c4a388a83f029f881ad099468a6531c30b0c716
MikaNishida <mika.nishida1130@gmail.com>
2017-11-02 18:17:49 +0900

    6th commit

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class UIController : MonoBehaviour {
+
+	// クリアor ゲームオーバーテキスト
+	private GameObject gameEnd;
+
+	// ゲームオーバーの判定
+	private bool isGameOver = false;
+
+
+	// Use this for initialization
+	void Start () {
+		// シーンビューからオブジェクトの実体を検索する
+		this.gameEnd  = GameObject.Find ("Text");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// ジェムの数を数える
+		int count = GameObject.FindGameObjectsWithTag ("GemTag").Length;
+
+		// ゲームクリアの場合
+		if(count == 0) {
+			GameClear ();
+		}
+	}
+
+	public void GameClear() {
+		this.gameEnd.GetComponent<Text> ().text = "Clear!";
+		this.isGameOver = true;
+	}
+}
