26654750ef314399e5e72e1a9984a720b0896252
MikaNishida <mika.nishida1130@gmail.com>
2017-11-02 12:01:33 +0900

    最終課題 3rd commit

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class MainCameraController : MonoBehaviour {
 
-	// Use this for initialization
+	// プレイヤーを入れる
+	private GameObject player;
+
+	private Vector3 distance = new Vector3(0, 2, -4);
+
+	private Vector3 lookPoint = new Vector3(0, 1.35f, 0);
+
 	void Start () {
-		
+		this.player = GameObject.FindWithTag ("Player");
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		this.transform.position = this.player.transform.position + distance;
+
+		Vector3 lookVector = this.player.transform.position + lookPoint - this.transform.position;
+		this.transform.rotation = Quaternion.LookRotation(lookVector);
 	}
 }
