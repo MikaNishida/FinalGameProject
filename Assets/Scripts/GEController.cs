0264d13c7c7a09830822c9b00d44b5085930f5fe
MikaNishida <mika.nishida1130@gmail.com>
2017-11-05 06:12:01 +0900

    9th commit

--- Assets/Scripts/GEController.cs
+++ Assets/Scripts/GEController.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class GEController : MonoBehaviour {
 
 	// カウンター
-	private float counter = 60f;
+	private float counter = 0f;
 
 	// スタートボタン
 	GameObject reStartButton;
+
+	// joystick
+	GameObject mobileController;
 
 	private bool isGameOver = false;
 
@@ -18,22 +21,28 @@ public class GEController : MonoBehaviour {
 		reStartButton = GameObject.Find ("reStartButton");
 		reStartButton.SetActive (false);
 
+		mobileController = GameObject.Find ("MobileSingleStickControl");
+		mobileController.SetActive (true);
 	}
 
 	void Update() {
 		counter += Time.deltaTime;
 		int count = GameObject.FindGameObjectsWithTag ("GemTag").Length;
 		// ゲームクリアの場合
-		if (count == 0 /*&& counter <= 60*/) {
+		if (count == 0 && counter <= 60) {
 			this.GetComponent<Text> ().text = "Clear!";
 			isGameOver = true;
-			reStartButton.SetActive (true);  
+			reStartButton.SetActive (true); 
+			mobileController.SetActive (false);
+
 		}
 
 		// ゲームオーバーの場合
-//		if(count > 0 && counter > 60) {
-//			this.GetComponent<Text> ().text = "Game Over!";
-//			isGameOver = true;
-//		}
+		if(count > 0 && counter > 60) {
+			this.GetComponent<Text> ().text = "Game Over!";
+			isGameOver = true;
+			reStartButton.SetActive (true);
+			mobileController.SetActive (false);
+		}
 	}
 }
--- Assets/Scripts/UnitychanController.cs
+++ Assets/Scripts/UnitychanController.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UnitychanController : MonoBehaviour {
-
+	
+	// gameEndを入れる
 	GameObject gameEnd;
+
+
+	// アニメーションするためのコンポーネントを入れる
+	private Animator myAnimator;
+
 
 	// Use this for initialization
 	void Start () {
 		this.gameEnd = GameObject.Find ("GameEnd");
+	
 	}
 	
 	// Update is called once per frame
@@ -27,6 +34,12 @@ public class UnitychanController : MonoBehaviour {
 		// Rocketと接触した時の処理
 		if(other.gameObject.tag == "RocketTag") {
 			gameEnd.GetComponent<Text> ().text = "Game Over !";
+		}
+
+		// 障害物の上でジャンプできるようにする
+		if(other.gameObject.tag == "ItemTag") {
+			this.myAnimator = GetComponent<Animator> ();
+			this.myAnimator.SetBool ("OnGround", true);
 		}
 	}
 }
